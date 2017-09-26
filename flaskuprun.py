25ae3590a5fa1b06cbeb47122c4c5e137374bd5a
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2017-09-26 17:09:05 +0100

    That's more like it, got the webpage to say when the next 262 is

--- flaskuprun.py
+++ flaskuprun.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     intest = tflapi.makedat()
-    return intest[1]
+    return 'A {} is arriving in {} minutes'.format(intest[0][0], intest[0][1])
