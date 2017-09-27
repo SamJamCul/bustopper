3c47c25828c6528047d661d83c086d130d879c58
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2017-09-27 17:05:48 +0100

    Added a weather page, and hooked it up accordingly with flasky python

--- flaskuprun.py
+++ flaskuprun.py
@@ -9,8 +9,12 @@ def index():
     return render_template('index.html')
 
 @app.route('/buses')
-def main():
+def buses():
     bustuff = tflapi.makedat()
     route = bustuff[0][0]
     time = bustuff[0][1]
     return render_template('buses.html', time=time, route=route)
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
