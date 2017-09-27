a8768bbf51ae34cf3327648dc98a0d6b4b32b47e
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2017-09-27 16:14:03 +0100

    Two pages now, index and a main - they link to each other helpfully

--- flaskuprun.py
+++ flaskuprun.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/index')
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 @app.route('/main')
 def main():
