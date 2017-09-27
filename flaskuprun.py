4c38d1dd955b51d05908655b080dbe84212a4f5c
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2017-09-27 11:50:14 +0100

    new main page in html

--- flaskuprun.py
+++ flaskuprun.py
@@ -1,8 +1,12 @@
 import tflapi
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
